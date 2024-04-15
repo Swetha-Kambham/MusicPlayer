9798174a8bbd0221d53aca66b8910f519911234c
imnites <nitesh110897@gmail.com>
2024-04-16 00:28:41 +0530

    fix play pause

--- frontend/src/PlayPauseButtons.js
+++ frontend/src/PlayPauseButtons.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { AiFillPlayCircle, AiFillPauseCircle } from 'react-icons/ai';
+import { FaStopCircle } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 import PropTypes from 'prop-types';
 
-export const PlayPauseButtons = ({ isPlaying, onPlayClick, onPauseClick }) => {
+export const PlayPauseButtons = ({
+  isPlaying,
+  isPaused,
+  onPlayClick,
+  onPauseClick,
+  onStopClick
+}) => {
   return (
     <>
       {!isPlaying ? (
@@ -19,14 +26,23 @@ export const PlayPauseButtons = ({ isPlaying, onPlayClick, onPauseClick }) => {
           </IconContext.Provider>
         </button>
       )}
+      {isPlaying || isPaused ? (
+        <button className="stopButton" onClick={onStopClick}>
+          <IconContext.Provider value={{ size: '4em', color: '#27AE60' }}>
+            <FaStopCircle />
+          </IconContext.Provider>
+        </button>
+      ) : null}
     </>
   );
 };
 
 PlayPauseButtons.propTypes = {
   isPlaying: PropTypes.bool,
+  isPaused: PropTypes.bool,
   onPrevClick: PropTypes.func,
   onPlayClick: PropTypes.func,
   onPauseClick: PropTypes.func,
-  onNextClick: PropTypes.func
+  onNextClick: PropTypes.func,
+  onStopClick: PropTypes.func
 };
--- frontend/src/usePlayControls.js
+++ frontend/src/usePlayControls.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState, useMemo, useEffect } from 'react';
 import songList from './mp3s.json';
 import { Howl } from 'howler';
 
@@ -10,12 +10,17 @@ const fetchSong = (label) => {
   }
 };
 
-export const usePlayControls = (label) => {
+export const usePlayControls = () => {
   const [id, setId] = useState(undefined);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [internalLabel, setInternalLabel] = useState(null);
-
+  const [isPaused, setIsPaused] = useState(false);
+  const [label, setLabel] = useState(null);
   const [sound, setSound] = useState(null);
+  const [seconds, setSeconds] = useState(0);
+  const [currTime, setCurrTime] = useState({
+    min: 0,
+    sec: 0
+  });
 
   const onPlayClick = useCallback(() => {
     if (sound) {
@@ -30,38 +35,104 @@ export const usePlayControls = (label) => {
     }
   }, [id, sound]);
 
-  useEffect(() => {
-    if (label !== internalLabel && !isPlaying) {
-      if (sound) {
-        sound.unload();
-      }
-
-      if (label && fetchSong(label)) {
-        const s = new Howl({
-          src: require(`./assets/mp3-songs/${label}/${fetchSong(label)}`),
-          onplay: () => {
-            setIsPlaying(true);
-          },
-          onpause: () => {
-            setIsPlaying(false);
-          },
-          onend: () => {
-            setIsPlaying(false);
-          }
-        });
-
-        const id = s.play();
-        setId(id);
-        setSound(s);
-        setInternalLabel(label);
-      }
+  const onStopClick = useCallback(() => {
+    if (sound) {
+      sound.stop(id || undefined);
     }
-  }, [internalLabel, isPlaying, label, sound]);
+  }, [id, sound]);
+
+  const onLabelChange = useCallback(
+    (newLabel) => {
+      if (!isPlaying && !isPaused) {
+        if (sound) {
+          sound.unload();
+        }
+
+        if (newLabel && fetchSong(newLabel)) {
+          const s = new Howl({
+            src: require('./assets/mp3-songs' + `/${newLabel}/${fetchSong(newLabel)}`),
+            onplay: () => {
+              setIsPlaying(true);
+              setIsPaused(false);
+            },
+            onpause: () => {
+              setIsPlaying(false);
+              setIsPaused(true);
+            },
+            onend: () => {
+              setIsPlaying(false);
+              setIsPaused(false);
+            },
+            onstop: () => {
+              setIsPlaying(false);
+              setIsPaused(false);
+            }
+          });
+
+          const id = s.play();
+          setId(id);
+          setSound(s);
+          setLabel(newLabel);
+        }
+      }
+    },
+    [isPaused, isPlaying, sound]
+  );
+
+  const time = useMemo(() => {
+    if (sound && id) {
+      const _duration = sound.duration();
+
+      const sec = Math.floor(_duration % 60);
+      const min = Math.floor(_duration / 60);
+
+      return {
+        min,
+        sec,
+        duration: Math.floor(_duration)
+      };
+    }
+
+    return null;
+  }, [sound, id]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound && id) {
+        setSeconds(sound.seek(id));
+        const min = Math.floor(sound.seek(id) / 60);
+        const sec = Math.floor(sound.seek(id) % 60);
+
+        setCurrTime({
+          min,
+          sec
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound, id]);
+
+  const onSeek = useCallback(
+    (e) => {
+      if (sound && id) {
+        sound.seek(id);
+        setSeconds(e.target.value);
+      }
+    },
+    [id, sound]
+  );
 
   return {
     isPlaying,
     onPlayClick,
     onPauseClick,
-    label: internalLabel
+    onStopClick,
+    label,
+    onLabelChange,
+    time,
+    seconds,
+    currTime,
+    onSeek,
+    isPaused
   };
 };
